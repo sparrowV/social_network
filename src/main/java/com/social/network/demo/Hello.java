b5ab32987edf157b31e7453c457fc0a3da8c667f
oto <odval14@freeuni.edu.ge>
2019-04-03 23:47:41 +0400

    initial

---
+++ src/main/java/com/social/network/demo/Hello.java
@@ -0,0 +1,15 @@
+package com.social.network.demo;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping({"/"})
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
